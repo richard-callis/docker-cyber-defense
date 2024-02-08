e9fc5dacfa8b20ec7a38ff0a7e3201c3693bfdb6
richard-callis <richard.callis@outlook.com>
2024-02-08 01:09:59 -0500

    Update hostname_analysis.py

--- python/hostname_analysis.py
+++ python/hostname_analysis.py
@@ -2,13 +2,14 @@ import pywars
 import re
 import pathlib
 # 01-Apr-2015 10:00:03.428 client 249.169.199.235#55766: query: talkgadget.google.com IN A + ((8.8.8.8))
+path = '/home/student/Public?log/dnslogs/'
 
 def lab330(the_data):
     #Open specified file from /home/student/Public/log/dnslogs
     #Retrieve all the IP addresses from the log specified in the_data
     #Pass that list to sorted() and return it
-
-    for eachline in open("query.log":
+    the_file, the_search = the_data
+    for eachline in open(path+the_file):
         c.update(re.findall(r"client .*?query: (\s+) IN", eachline))
       
     pass
